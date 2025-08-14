dbf5192e418d442d74614a84c41158742aae62a7
Janosch Dickhage <blockplacer4@gmail.com>
2025-08-14 16:11:20 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,28 +8,12 @@ export default async ({ req, res, log, error }) => {
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+  const user = new User(client);
 
   try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
+    const response = await users.delete(userId);
+    log(`Response of User deletion: ${response.total}`);
   } catch(err) {
-    error("Could not list users: " + err.message);
+    error("Could not delete User Account: " + err.message);
   }
-
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
-
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
 };
